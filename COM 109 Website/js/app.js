72802c812e50d9f36a8cc4578a1cffd0c05fa9e8
Joecat <147674544+dev-joecat@users.noreply.github.com>
2023-10-23 01:31:57 +0100

    Add files via upload

---
+++ COM 109 Website/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('#HTML').hover(function() {
+        $(this).css(
+        {
+            'transform'             : 'rotate(30deg) translate(100px, 45px) skew(35deg, 0deg)'
+        }
+        );
+    });
+
+    $('#HQ').hover(function() {
+        $(this).css(
+        {
+            'transform'             : 'rotate(30deg) translate(100px, 45px) skew(35deg, 0deg)'
+        }
+        );
+    });
+
+    $('#SSA2').hover(function() {
+        $(this).css(
+        {
+            'transform'             : 'rotate(30deg) translate(100px, 45px) skew(35deg, 0deg)'
+        }
+        );
+    });
+
+    $('#VEND').hover(function() {
+        $(this).css(
+        {
+            'transform'             : 'rotate(30deg) translate(100px, 45px) skew(35deg, 0deg)'
+        }
+        );
+    });
+});
+
+
+
